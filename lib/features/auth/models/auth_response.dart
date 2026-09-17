d04917f2ce3c9b4914c62fd1bb02f3ae52edd3f6
roypa123 <roypa81130@gmail.com>
2026-09-17 18:04:11 +0530

    fix: some changes

---
+++ lib/features/auth/models/auth_response.dart
@@ -0,0 +1,13 @@
+class AuthResponse {
+  final String accessToken;
+  final String refreshToken;
+
+  const AuthResponse({required this.accessToken, required this.refreshToken});
+
+  factory AuthResponse.fromJson(Map<String, dynamic> json) {
+    return AuthResponse(
+      accessToken: json['accessToken'] as String,
+      refreshToken: json['refreshToken'] as String,
+    );
+  }
+}
